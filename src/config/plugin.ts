edbd455333e602958b60b1d19a978dede41fc440
fox <firefoxdraging@gmail.com>
2021-06-15 15:38:51 +0800

    rmve mysql plugin

--- src/config/plugin.ts
+++ src/config/plugin.ts
@@ -2,10 +2,6 @@ import { EggPlugin } from 'egg';
 export default {
   logrotator: false, // disable when use @midwayjs/logger
   static: true,
-  mysql: {
-    enable: true,
-    package: 'egg-mysql'
-  },
   redis: {
     enable: true,
     package: 'egg-redis'
